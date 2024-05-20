ca99f154fe47bfac249204aa51db56422ce4fc47
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-05-20 15:17:57 +0700

    added structured calculating a * b

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+    private int a;
+    private int b;
+
+    public App(int a, int b) {
+        this.a = a;
+        this.b = b;
+    };
+
+    public int get() {
+        return this.a * this.b;
+    };
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        App sum = new App(a, b);
+        
+        System.out.println(sum.get());
+    };
+};
---
+++ src/nonoop/Calculator.java
@@ -0,0 +1,11 @@
+package nonoop;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int c = a * b;
+        System.out.println(c);
+        return;
+    };
+}
