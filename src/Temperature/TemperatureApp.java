a3c1652e0c0659afcbe9b48324d10e1b14988ce7
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-07-06 14:50:53 +0700

    p

--- src/Temperature/TemperatureApp.java
+++ src/Temperature/TemperatureApp.java
@@ -5,8 +5,8 @@ import Temperature.commandprocessor.CommandProcessor;
 public class TemperatureApp {
     public static void main(String[] args) {
         TemperatureModel temperatureModelRemote = new TemperatureModel();
-        CommandProcessor commandProcessor = CommandProcessor.makeCommandProcessor();
-        TemperatureView temperatureViewRemote = new TemperatureView(temperatureModelRemote, commandProcessor);
+        CommandProcessor commandProcessorRemote = CommandProcessor.makeCommandProcessor();
+        TemperatureView temperatureViewRemote = new TemperatureView(temperatureModelRemote, commandProcessorRemote);
 
         temperatureViewRemote.Show();
     }
